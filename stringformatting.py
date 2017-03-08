82620522725374ac40f2cfe937301437c1b986c1
Xavier Bohorquez <bohorqux@bu.edu>
2017-03-08 17:08:27 -0500

    Added docstrings, comments, and cleaned up code

--- stringformatting.py
+++ stringformatting.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 import re
 
-def lead_zeros(string, amount):
-    return "0" * (amount - len(string)) + string
+def lead_zeros(string, num_zeros):
+    """ pads zero(s) to front of specified string """
+    return "0" * (num_zeros - len(string)) + string
 
 def miliTime(time):
+    """ converts meridian time string into military time string """
+    
     time = re.sub(":", "", time)
     isPM = False
-    
     if "pm" in time:
         isPM = True
 
-    time = int(time[:-2])
-    
+    time = int(time[:-2]) #removing am/pm discretion to perform essential mathematical operations
     if 1159 < time < 1259 and not isPM:
         time -= 1200        
     if isPM:
@@ -20,19 +21,29 @@ def miliTime(time):
     
     return str(time)
 
-def string_to_date(string):
+def string_to_date(timestamp):
+    """ converts messenger timestamp string into its integer representation 
+
+        Keyword Arguments:
+
+        string -- input must be as follows: {DayofWeek, MonthandDay, Year at Time EDT}
+    """
+    #initialize dictionary to correspond Month with integer
     months = dict()
     m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
     for i in range(len(m)):
         months[m[i]] = i
-        
-    stored = string.split(" ")[1:]
+
+    #format Messenger timestamp to remove irrelevant information
+    stored = timestamp.split(" ")[1:]
     stored[0] = str(months[stored[0]])
     stored[1] = re.sub(",", "", stored[1])
     processed = [stored[2], lead_zeros(stored[0], 2), lead_zeros(stored[1], 2), lead_zeros(miliTime(stored[4]), 4)]
-    timestamp = int("".join(processed))
+
+    #devise integer out of resulting timestamp string
+    timestamp_int = int("".join(processed))
     
-    return timestamp
+    return timestamp_int
 
 
         
--- threadfunctions.py
+++ threadfunctions.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
+from stringformatting import *
+from bs4 import BeautifulSoup
 
 def getUniqueUsers(chat):
+    """ return distinct list of users that exist in a single thread/chat """
     users = [user.string for user in chat.find_all("span", class_ = "user")]
     return list(set(users))
 
-def displayUsers():
-    for i in range(len(all_unique_users)):
+def displayUsers(chat):
+    unique_users = getUniqueUsers(chat)
+    for u in unique_users:
+        if "facebook.com" in u:
+            print("UNKNOWN...")
+        else:
+            print(u)
+    return 0
+    
+def displayAllUsers(threads):
+    """ displays all users involved in each thread """
+    for i in range(len(threads)):
        print("thread[%d]:----" % i)
-       
-       for u in all_unique_users[i]:
-          if "facebook.com" in u:
-             print("UNKNOWN...")
-          else:
-             print(u)
-             
+       displayUsers(thread[i])
+
        print("\n")
     return 0
 
