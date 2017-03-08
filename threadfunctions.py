cb24a56c9ef1e31df32ed94714408b9d6e62219f
Xavier Bohorquez <bohorqux@bu.edu>
2017-03-08 18:10:42 -0500

    updated display_ functions

--- threadfunctions.py
+++ threadfunctions.py
@@ -2,6 +2,19 @@
 from stringformatting import *
 from bs4 import BeautifulSoup
 
+def initThreads(messenger_html_doc):
+    """ devises soup object out of specified messenger file """
+    print("Opening file: %s" % messenger_html_doc)
+    fileIn = open(messenger_html_doc, "r")
+    feed = fileIn.read()
+    fileIn.close()
+    print("Converting %s to soup object" % messenger_html_doc)
+    soup = BeautifulSoup(feed, 'lxml')
+    threads = soup.find_all("div", class_ = "thread")
+
+    print("Threads parsed successfully!")
+    return threads
+
 def getUniqueUsers(chat):
     """ return distinct list of users that exist in a single thread/chat """
     users = [user.string for user in chat.find_all("span", class_ = "user")]
@@ -20,8 +33,17 @@ def displayAllUsers(threads):
     """ displays all users involved in each thread """
     for i in range(len(threads)):
        print("thread[%d]:----" % i)
-       displayUsers(thread[i])
+       displayUsers(threads[i])
 
        print("\n")
     return 0
+
+
+##################################################################################
+def main():
+    threads = initThreads("messages.htm")
+    displayAllUsers(threads)
+    return 0
+
+main()
 
